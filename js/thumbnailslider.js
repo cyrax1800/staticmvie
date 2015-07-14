2f87ca3fcb53ff622c0df10be8f8b2547d266159
Michael <mchen0358@gmail.com>
2015-07-14 19:32:49 +0700

    statikmvie

---
+++ js/thumbnailslider.js
@@ -0,0 +1,93 @@
+$(document).ready(function() {
+	$('#tnail1').carousel({
+	interval: 10000
+	})
+    
+    $('#tnail1').on('slid.bs.carousel', function() {
+    	//alert("slid");
+	});
+});
+
+$(document).ready(function() {
+	$('#tnail2').carousel({
+	interval: 10000
+	})
+    
+    $('#tnail2').on('slid.bs.carousel', function() {
+    	//alert("slid");
+	});
+});
+
+$(document).ready(function() {
+	$('#tnail3').carousel({
+	interval: 10000
+	})
+    
+    $('#tnail3').on('slid.bs.carousel', function() {
+    	//alert("slid");
+	});
+});
+$(document).ready(function() {
+	$('#tnail4').carousel({
+	interval: 10000
+	})
+    
+    $('#tnail4').on('slid.bs.carousel', function() {
+    	//alert("slid");
+	});
+});
+$(document).ready(function() {
+	$('#tnail5').carousel({
+	interval: 10000
+	})
+    
+    $('#tnail5').on('slid.bs.carousel', function() {
+    	//alert("slid");
+	});
+});
+$(document).ready(function() {
+	$('#tnail6').carousel({
+	interval: 10000
+	})
+    
+    $('#tnail6').on('slid.bs.carousel', function() {
+    	//alert("slid");
+	});
+});
+$(document).ready(function() {
+	$('#tnail7').carousel({
+	interval: 10000
+	})
+    
+    $('#tnail7').on('slid.bs.carousel', function() {
+    	//alert("slid");
+	});
+});
+$(document).ready(function() {
+	$('#tnail8').carousel({
+	interval: 10000
+	})
+    
+    $('#tnail8').on('slid.bs.carousel', function() {
+    	//alert("slid");
+	});
+});
+$(document).ready(function() {
+	$('#tnail9').carousel({
+	interval: 10000
+	})
+    
+    $('#tnail9').on('slid.bs.carousel', function() {
+    	//alert("slid");
+	});
+});
+
+$(document).ready(function() {
+	$('#tnail10').carousel({
+	interval: 10000
+	})
+    
+    $('#tnail10').on('slid.bs.carousel', function() {
+    	//alert("slid");
+	});
+});
